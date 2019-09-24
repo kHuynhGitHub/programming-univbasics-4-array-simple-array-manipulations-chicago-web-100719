1cbc086234f0b26615b0322a7676f0a3dcf2cd6e
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 03:11:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,47 @@
-def using_push ()
+def using_push(Array, String)
+  
+end
+
+def using_unshift(Array, String)
+  
+end
+
+def using_pop(Array)
+  
+end
+
+def pop_with_args(Array)
+  
+end
+
+def using_shift(Array)
+  
+end
+
+def shifts_with_args(Array)
+  
+end
+
+def using_concat()
+  
+end
+
+def using_insert(Array)
+  
+end
+
+def using_uniq(Array)
+  
+end
+
+def using_flatten(Array)
+  
+end
+
+def using_delete(Array, String)
+  
+end
+
+def using_delete_at(Array, Integer)
+  
+end
